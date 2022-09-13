3843ebf084432a38e02dd3696727ba2e7f0c5803
Nathan <nathansilva406@gmail.com>
2022-09-13 00:42:54 -0300

    atualizado

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 
 const Clients = {
-    id: "3",
-    nome: "manu",
-    email: "manu@gmail.com",
+    id: "4",
+    nome: "lucas",
+    email: "lucas@gmail.com",
     idade: "22",
     senha: "1234"
 }
@@ -14,9 +14,10 @@ const createClient = (client) => {
      const dados = JSON.parse(localStorage.getItem('client')) ?? [] ;
      dados.push(client);
      localStorage.setItem('client', JSON.stringify(dados));
-
-    
-
 };
+
+//read
+
+const readClient = () => localStorage.getItem('client') 
 
 
